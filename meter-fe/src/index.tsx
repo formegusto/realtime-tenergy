de685614452087b111938d03ddc48dfb86cd2206
formegusto <formegusto@gmail.com>
2022-08-11 16:57:27 +0900

    meter update : init package setting

--- meter-fe/src/index.tsx
+++ meter-fe/src/index.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
+import GlobalStyles from "@styles/GlobalStyles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { RecoilRoot } from "recoil";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <GlobalStyles />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </QueryClientProvider>
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
