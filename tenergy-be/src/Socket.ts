584f57342094ea6f4e57dbd4c0392e3b17981c89
formegusto <formegusto@gmail.com>
2022-08-25 17:55:04 +0900

    socket : token check test

--- tenergy-be/src/Socket.ts
+++ tenergy-be/src/Socket.ts
@@ -1,6 +1,8 @@
 import http from "http";
 import express from "express";
 import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
+import { MonthMeterDataModel } from "./models";
 
 function SocketConnect(server: http.Server, app: express.Application) {
   const io = new Server(server, {
@@ -12,9 +14,22 @@ function SocketConnect(server: http.Server, app: express.Application) {
   });
 
   app.set("io", io);
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     console.log(`----[Alert.IO] Socket Connection :)----`);
-    console.log(socket);
+    console.log(socket.request.headers["authorization"]);
+    const token = socket.request.headers["authorization"]!;
+    const secret = process.env.JWT_SECRET!;
+    const { household } = jwt.verify(token, secret) as any;
+    console.log(household);
+
+    const monthMeterData = await MonthMeterDataModel.updateOne({
+      name: household.name,
+    });
+    console.log(monthMeterData);
+
+    socket.on("test", () => {
+      console.log(socket.request.headers["authorization"]);
+    });
 
     socket.on("disconnect", () => {
       console.log(`----[Alert.IO] Socket DisConnection :)----`);
