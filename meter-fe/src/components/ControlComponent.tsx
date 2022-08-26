3bd2f2953bcb24a3ff3286adfab510175c44efe8
formegusto <formegusto@gmail.com>
2022-08-26 13:45:17 +0900

    meter update : auto next, prev

--- meter-fe/src/components/ControlComponent.tsx
+++ meter-fe/src/components/ControlComponent.tsx
@@ -3,14 +3,48 @@ import { Button, Stack } from "@mui/material";
 type Props = {
   next: () => void;
   prev: () => void;
+  autoNext: boolean;
+  setAutoNext: (state: boolean) => void;
+  autoPrev: boolean;
+  setAutoPrev: (state: boolean) => void;
 };
 
-function ControlComponent({ next, prev }: Props) {
+function ControlComponent({
+  next,
+  prev,
+  autoNext,
+  setAutoNext,
+  autoPrev,
+  setAutoPrev,
+}: Props) {
   return (
     <Stack direction="row" justifyContent="space-between" p={2}>
       <Button variant="contained" onClick={prev}>
         prev
       </Button>
+
+      {!autoNext &&
+        (autoPrev ? (
+          <Button variant="contained" onClick={() => setAutoPrev(false)}>
+            auto-prev cancle
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={() => setAutoPrev(true)}>
+            auto-prev
+          </Button>
+        ))}
+
+      {!autoPrev &&
+        (autoNext ? (
+          <Button variant="contained" onClick={() => setAutoNext(false)}>
+            auto-next cancle
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={() => setAutoNext(true)}>
+            auto-next
+          </Button>
+        ))}
+
       <Button variant="contained" onClick={next}>
         next
       </Button>
--- meter-fe/src/containers/ControlContainer.tsx
+++ meter-fe/src/containers/ControlContainer.tsx
@@ -6,6 +6,8 @@ import React from "react";
 import { useRecoilState } from "recoil";
 
 function ControlContainer() {
+  const [autoNext, setAutoNext] = React.useState<boolean>(false);
+  const [autoPrev, setAutoPrev] = React.useState<boolean>(false);
   const queryClient = useQueryClient();
   const [token, setTokenState] = useRecoilState(tokenState);
   const nextMutate = useMutation(nextControl, {
@@ -29,7 +31,40 @@ function ControlContainer() {
     if (token) prevMutate.mutate(token);
   }, [prevMutate, token]);
 
-  return token ? <ControlComponent next={next} prev={prev} /> : <></>;
+  React.useEffect(() => {
+    let intervalId: NodeJS.Timer | null = null;
+    if (autoNext) {
+      intervalId = setInterval(next, 1000);
+    }
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [autoNext, next]);
+
+  React.useEffect(() => {
+    let intervalId: NodeJS.Timer | null = null;
+    if (autoPrev) {
+      intervalId = setInterval(prev, 1000);
+    }
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [autoPrev, prev]);
+
+  return token ? (
+    <ControlComponent
+      next={next}
+      prev={prev}
+      autoNext={autoNext}
+      setAutoNext={setAutoNext}
+      autoPrev={autoPrev}
+      setAutoPrev={setAutoPrev}
+    />
+  ) : (
+    <></>
+  );
 }
 
 export default ControlContainer;
